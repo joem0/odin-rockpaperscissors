23d8e43d7d24dd8baf8e76367555eb079f2f4d98
Joseph Monoenko <joemonoenko@gmail.com>
2024-03-20 15:35:31 -0400

    added basic playGameLogic

--- rps.js
+++ rps.js
@@ -1,10 +1,31 @@
 let choices = ["Rock","Paper","Scissors"];
 let getComputerChoice = value => choices[Math.floor(Math.random() * choices.length)];
-function playRound(playerSelection,computuerSelection) {
-    return playerSelection === "paper" && computuerSelection === "rock" ? "player"
-    : playerSelection === "rock" && computuerSelection === "scissors" ? "player"
-    : playerSelection === "scissors" && computuerSelection ==="paper" ? "player"
+function playRound(playerSelection,computerSelection) {
+    return playerSelection === "paper" && computerSelection === "rock" ? "player"
+    : playerSelection === "rock" && computerSelection === "scissors" ? "player"
+    : playerSelection === "scissors" && computerSelection ==="paper" ? "player"
     : playerSelection === computerSelection ? "tie"
     : "computer";
 }
-console.log(playRound("paper","paper"));
+
+const numRounds = 1;
+function playGame() {
+    for (let i = 0; i < numRounds; i++) {
+        // const computerChoice = getComputerChoice();
+        // const playerChoice = prompt();
+        console.log("lets play");
+        const computerChoice = "rock";
+        const playerChoice = "paper";
+        const result = playRound(playerChoice,computerChoice);
+        let resultStr = "";
+        switch (result) {
+            case 'player':
+                resultStr = `You win! ${playerChoice} beats ${computerChoice}`;
+            case 'computer':
+            case 'tie':
+        }
+        console.log(resultStr);
+    }
+}
+
+playGame()
