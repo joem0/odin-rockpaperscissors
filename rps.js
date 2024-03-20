b95e2c7607774c8e2910a87cc88412d5632b94ba
Joseph Monoenko <joemonoenko@gmail.com>
2024-03-20 16:05:42 -0400

    added additional logic to play game, validate user input. Use Alerts instead of console.log for ease of playing

--- rps.js
+++ rps.js
@@ -1,6 +1,8 @@
-let choices = ["Rock","Paper","Scissors"];
+let choices = ["rock","paper","scissors"];
 let getComputerChoice = value => choices[Math.floor(Math.random() * choices.length)];
 function playRound(playerSelection,computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
     return playerSelection === "paper" && computerSelection === "rock" ? "player"
     : playerSelection === "rock" && computerSelection === "scissors" ? "player"
     : playerSelection === "scissors" && computerSelection ==="paper" ? "player"
@@ -8,24 +10,51 @@ function playRound(playerSelection,computerSelection) {
     : "computer";
 }
 
-const numRounds = 1;
+
+
+function validateInput(text,array) {
+    return array.includes(text,0)
+}
+
+let capitalize = text => text[0].toUpperCase() + text.slice(1);
+
+const numRounds = 5;
+
 function playGame() {
+    let playerWins = 0;
+    let computerWins = 0;
+    alert("Welcome to Rock, Paper, Scissors versus a computer. You will play 5 rounds.")
     for (let i = 0; i < numRounds; i++) {
-        // const computerChoice = getComputerChoice();
-        // const playerChoice = prompt();
-        console.log("lets play");
-        const computerChoice = "rock";
-        const playerChoice = "paper";
+        let computerChoice = getComputerChoice();
+        let playerChoice = prompt("Please input Rock,Paper, or Scissors:");
+
+        while (!validateInput(playerChoice.toLowerCase(),choices)) {
+            playerChoice = prompt("Invalid input. Please input Rock, Paper or Scissors:")
+        }
+
         const result = playRound(playerChoice,computerChoice);
+        computerChoice = capitalize(computerChoice);
         let resultStr = "";
         switch (result) {
             case 'player':
                 resultStr = `You win! ${playerChoice} beats ${computerChoice}`;
+                playerWins++;
+                break;
             case 'computer':
+                resultStr = `Computer win! ${computerChoice} beats ${playerChoice}`;
+                computerWins++;
+                break;
             case 'tie':
+                resultStr = `Tie! You and the comptuer chose: ${computerChoice}`;
+                break;
         }
-        console.log(resultStr);
+        alert(resultStr);
     }
+    let results = (playerWins,computerWins) => playerWins > computerWins ? `You beat the computer!`
+        : computerWins > playerWins ? "The comptuer won!"
+        : "You tied the computer!"
+    
+    return alert(results(playerWins,computerWins) + ` Final Score - You: ${playerWins} Computer: ${computerWins}`);
 }
 
-playGame()
+playGame();
